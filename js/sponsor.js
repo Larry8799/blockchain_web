47aed9fdaf91e2c456bd92d97feb46a0c2ea74f0
BillKung <billkung.happy@gmail.com>
2020-06-23 00:59:15 +0800

    fix sponsor

--- js/sponsor.js
+++ js/sponsor.js
@@ -3,7 +3,6 @@
 // Gold, Sliver, Media 以此類推 G0.png, S1.png, M0.png 等
 // 須確保所有 logo 都是png檔，且必須從0開始，不能斷號
 // 完成後將底下 num_of_sponsor 的值改成sponsors的總數（對應每個）
-// 填寫 all_contactor_info, id 對應name，title, email
 // 完成！！！
 // PS. 原本 img/sponsors/ 內應該就有很多張，請先刪掉，那是我測試用的
 
@@ -11,14 +10,14 @@ var num_of_sponsor=[3,1,1,1]; // 改這裡
 var code_of_sponsor=['D','G','S','M'];
 
 // 
-var all_contactor_info={
-	'D0':['Alice','Product Manager','alice@gmail.com'],
-	'D1':['Bob','CEO','bob@gmail.com'],
-	'D2':['Ken','Product Manager','ken@gmail.com'],
-	'G0':['George','Backend Engineer','geo@gmail.com'],
-	'S0':['Selina','Front Engineer','sel@gmail.com'],
-	'M0':['Mark','Full-Stack Engineer','mark@gmail.com'],
-};
+// var all_contactor_info={
+// 	'D0':['Alice','Product Manager','alice@gmail.com'],
+// 	'D1':['Bob','CEO','bob@gmail.com'],
+// 	'D2':['Ken','Product Manager','ken@gmail.com'],
+// 	'G0':['George','Backend Engineer','geo@gmail.com'],
+// 	'S0':['Selina','Front Engineer','sel@gmail.com'],
+// 	'M0':['Mark','Full-Stack Engineer','mark@gmail.com'],
+// };
 
 
 
@@ -32,11 +31,11 @@ for (var i = 0; i < 4; i++) {
 	}
 }
 
-$( ".sponsors_list img" ).click(function() {
-	$('#contact-modal-btn').click();
-	var info = all_contactor_info[$(this).attr('id')];
-	console.log(info);
-	$('#contact-name').html(info[0]);
-	$('#contact-title').html(info[1]);
-	$('#contact-email').html(info[2]);
-});
+// $( ".sponsors_list img" ).click(function() {
+// 	$('#contact-modal-btn').click();
+// 	var info = all_contactor_info[$(this).attr('id')];
+// 	console.log(info);
+// 	$('#contact-name').html(info[0]);
+// 	$('#contact-title').html(info[1]);
+// 	$('#contact-email').html(info[2]);
+// });
